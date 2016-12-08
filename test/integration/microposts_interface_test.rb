43e374a0991017bf1a624b54092560228f28b48d
Hisashi Kamezawa <hkame@outlook.jp>
2016-12-08 18:49:19 +0900

    introduce kaminari

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -9,7 +9,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   test "micropost interface" do
     log_in_as(@user)
     get root_path
-    assert_select 'div.pagination'
+    assert_select 'nav.pagination'
     # 無効な送信
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "" } }
@@ -25,7 +25,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_match content, response.body
     # 投稿を削除する
     assert_select 'a', text: 'delete'
-    first_micropost = @user.microposts.paginate(page: 1).first
+    first_micropost = @user.microposts.page(1).first
     assert_difference 'Micropost.count', -1 do
       delete micropost_path(first_micropost)
     end
